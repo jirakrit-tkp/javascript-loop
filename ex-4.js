b900f4e568b1d64118a92e597a9a76424a09b804
En <graphic.mimage@gmail.com>
2025-07-21 14:08:45 +0700

    fsd9-en

--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,8 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
-
+for ( const key in restaurants ){
+    const newName = "Restaurant Name: " + restaurants[key];
+    newRestaurants.push(newName);
+}
 console.log(newRestaurants);
