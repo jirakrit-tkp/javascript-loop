141ca8e14c4ba46f2f434772b5019522950f9695
Jirakrit T. <jirakrit.tkp@gmail.com>
2025-07-21 13:29:28 +0700

    ex-1--6

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,14 @@
 let scores = [100, 20, 3, 1000];
 let minScore;
 // Start coding here
+for (let eachScore of scores) {
+    if (minScore) {
+        if (eachScore < minScore) {
+            minScore = eachScore;
+        }
+    } else {
+        minScore = eachScore;
+    }
+}
 
 console.log(minScore);
