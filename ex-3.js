1e6bc3d362be178db8b68972e4e1b87cd74bfc01
Patchara <sombrousnorn@gmail.com>
2025-07-21 14:43:44 +0700

    test:loop

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,11 @@
 let scores = [100, 20, 3, 1000];
 let minScore;
 // Start coding here
-
+for (let i = 0; i < scores.length; i++) {
+    if (minScore > scores[i]) {
+        minScore = scores[i];
+    } else if (typeof minScore !== "number") {
+        minScore = scores[i];
+    }
+}
 console.log(minScore);
--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,7 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
-
+for (let i in restaurants) {
+    newRestaurants[i] = "Restaurant Name: " + restaurants[i];
+}
 console.log(newRestaurants);
